cb0d273c735a6cf2549c8238eedb799558266600
hninmabalo <hninmabalo@gmail.com>
2022-07-25 23:58:28 -0700

    add: creating html and css

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("space");
+
+const game = document.getElementById("game");
+const score = document.getElementById("score");
+const start = document.getElementById("start");
+const reset = document.getElementById("reset");
+
+const ctx = game.getContext("2d");
+
+game.setAttribute("height", getComputedStyle(game)["height"]);
+game.setAttribute("width", getComputedStyle(game)["width"]);
+
+
+
+window.addEventListener("DOMContentLoaded", function() {
+
+})
