fa866f99d2b52a1b69595f26fb0f91dfd0c06d33
hninmabalo <hninmabalo@gmail.com>
2022-07-30 14:21:27 -0700

    adding images to js

--- js/app.js
+++ js/app.js
@@ -6,83 +6,41 @@ const health = document.getElementById("health");
 const start = document.getElementById("start");
 const reset = document.getElementById("reset");
 const movement = document.getElementById("movement");
+const playerimg = document.getElementById("playerimg");
+const enemyimg = document.getElementById("enemyimg");
+const bulletimg = document.getElementById("bulletimg");
 
 const ctx = game.getContext("2d");
 let astronaut;
-let monster;
+// let monster;
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    astronaut = new Player(20, 80, playerimg, 70, 80);
+    alien = new Player(300, 200, enemyimg, 60, 80);
+
+    const runGame = setInterval(gameLoop, 60);
+});
+document.addEventListener("keydown", movementHander);
 
 game.setAttribute("width", getComputedStyle(game)["width"]);
 game.setAttribute("height", getComputedStyle(game)["height"]);
 
-window.addEventListener("DOMContentLoaded", function() {
-    astronaut = new Player(10, 20, "grey", 20, 20);
-
-    const runGame = setInterval(gameLoop, 60);
-});
-
 class Player {
-    constructor(x, y, color, width, height) {
+    constructor(x, y, image, width, height) {
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.image = image;
         this.width = width;
         this.height = height;
         this.alive = true;
         
 
         this.render = function() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, image1, this.width, this.height);
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         }
     }
 } 
-
-class Opponents {
-    constructor() {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = 1.5;
-        this.alive = true;
-
-        this.render = function() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y -= this.speed, this.width, this.height);
-        }
-    }
-
-}
-
-// const monster = new Opponents()
-
-class Projectile {
-    constructor(x, y, color, speed, width, height) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.speed =- 1.5;
-        this.width = width;
-        this.height = height;
-
-        this.render = function() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-
-
-}
-
-function addNewMonster() {
-    setTimeout(function() {
-        monster.alive = true;
-        let x = Math.floor(Math.random() * game.width) - 40;
-        let y = Math.floor(Math.random() * game.height) - 80;
-        monster = new Character(x, y, "green", 40, 50);
-    }, 500);
-    return true;
-}
 
 function movementHander(e) {
     console.log("movement", e.key);
@@ -92,7 +50,7 @@ function movementHander(e) {
             astronaut.y - 30 >= 0 ? (astronaut.y -= 10) : null;
             break;
         case "ArrowLeft":
-            astronaut.x - 1 >= 0 ? (astronaut.x -= 10) : null;
+            astronaut.x - 10 >= 0 ? (astronaut.x -= 10) : null;
             break;
         case "ArrowDown":
             astronaut.y + 10 <= game.height ? (astronaut.y += 10) : null;
@@ -100,39 +58,49 @@ function movementHander(e) {
         case "ArrowRight":
             astronaut.x + 10 <= game.width ? (astronaut.x += 10) : null;
             break;
+
+        case "w":
+            alien.y - 30 >= 0 ? (alien.y -= 10) : null;
+            break;
+        case "a":
+            alien.x - 10 >= 0 ? (alien.x -= 10) : null;
+            break;
+        case "s":
+            alien.y + 10 <= game.height ? (alien.y += 10) : null;
+            break;
+        case "d":
+            alien.x + 10 <= game.width ? (alien.x += 10) : null;
+            break;
+        
     }
 }
+
 
 function gameLoop() {
     ctx.clearRect(0, 0, game.width, game.height);
-    
-    if (monster.alive) {
-        monster.render();
 
-        let hit = detectHit(astronaut, monster);
-    }
-    astronaut.render();
+    astronaut.render();  
+    alien.render();
 
 }
 
 
 
 
-function detectHit(player1, player2) {
-    let hitTest = 
-        player1.y + player1.height > player2.y && 
-        player1.y < player2.y + player2.height &&
-        player1.x + player1.width > player2.x &&
-        player1.x < player2.x + player2.width; 
+// function detectHit(player1, player2) {
+//     let hitTest = 
+//         player1.y + player1.height > player2.y && 
+//         player1.y < player2.y + player2.height &&
+//         player1.x + player1.width > player2.x &&
+//         player1.x < player2.x + player2.width; 
 
-    if (hitTest) {
-        let newScore = Number(score.textContent) + 10;
-        score.textContent = newScore;
+//     if (hitTest) {
+//         let newScore = Number(score.textContent) + 10;
+//         score.textContent = newScore;
 
-        return addNewMonster();
-    } else {
-        return false;
-    }
-}
+//     } else {
+//         return false;
+//     }
+// }
 
  
